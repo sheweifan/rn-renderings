ca0f69465de944b594664d3b085e59eaf9693880
dolor <923986234@qq.com>
2017-08-30 21:48:10 +0800

    react-navigation remove header

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -15,11 +15,12 @@ const HeaderSearch = ()=>{
 
 class Search extends React.Component{
   static navigationOptions = {
-    headerTitle: <HeaderSearch /> ,
-    headerStyle: {
-      width: 500,
-      height: 50
-    }
+    // headerTitle: <HeaderSearch /> ,
+    // headerStyle: {
+    //   width: 500,
+    //   height: 50,
+    // }
+    header: null
   }
   // constructor(props){
   //   super(props);
