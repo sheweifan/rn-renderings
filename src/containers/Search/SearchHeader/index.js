7a4a8bb3a9339030c5e10b9a78ac36c116bd29e8
weifan she <923986234@qq.com>
2017-09-01 11:17:52 +0800

    search header backup

--- src/containers/Search/SearchHeader/index.js
+++ src/containers/Search/SearchHeader/index.js
@@ -10,15 +10,19 @@ import {
   Modal } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import Global from '../../../config/global.js';
+
 class SearchHeader extends React.Component{
   constructor(props){
     super(props);
     this.state ={
       pageX: 500,
-      pageY: 500
+      pageY: 500,
+      modelShow: false
     }
   }
   render(){
+    const { pageX, pageY, modelShow } = this.state;
     return (
       <View style={styles.header}>
 
@@ -34,7 +38,7 @@ class SearchHeader extends React.Component{
             <Text style={styles.selectText}>单图单</Text>
             <View
               style={styles.iconOuter} 
-              ref='selectTarget'
+              ref="selectTarget"
             >
               <Icon 
                 name="ios-arrow-down" 
@@ -60,15 +64,18 @@ class SearchHeader extends React.Component{
         <Modal
           animationType="none"
           transparent={true}
-          visible={true}
+          visible={modelShow}
+          onRequestClose={() => {}}
+        >
+          <View 
+            style={styles.selectMask} 
+            ref="model"
           >
-
-          <View style={styles.selectMask}></View>
-          <View style={[styles.iconOuter,styles.closeSelect,{
-            left: this.state.pageX,          
-            top: this.state.pageY         
-          }]}>
-            <Icon name="md-close" size={18} color="#fff" />
+            <View style={[styles.iconOuter,styles.closeSelect,{
+              marginLeft: pageX
+            }]}>
+              <Icon name="md-close" size={18} color="#fff" />
+            </View>
           </View>
         </Modal>
       </View>
@@ -89,36 +96,35 @@ class SearchHeader extends React.Component{
     // this.refs.selectTarget.measure((x,y,width,height,pageX,pageY)=>{
     //     console.log(x,y,width,pageX,pageY,height);
     // })
-    setTimeout(()=>{
-      this.refs.selectTarget.measure((x,y,width,height,pageX,pageY)=>{
-        console.log(pageX,pageY);
-        this.setState({
-          pageX,
-          pageY
-        })
-      })
-    },1500)
+    // setTimeout(()=>{
+    //   this.refs.selectTarget.measure((x,y,width,height,pageX,pageY)=>{
+    //     this.setState({
+    //       pageX,
+    //       pageY
+    //     })
+    //   })
+    // },1500)
   }
   selectChange(){
     // var tag = findNodeHandle(this.refs.selectTarget)
     // console.log(this.selectTarget);
     // console.log(findNodeHandle(this.selectTarget));
     // console.log(this.selectIcon)
-    
     this.refs.selectTarget.measure((x,y,width,height,pageX,pageY)=>{
-        console.log(pageX,pageY);
-        this.setState({
-          pageX,
-          pageY
-        })
+      this.setState({
+        modelShow: true,
+        pageX
+      });
     })
+
   }
 }
-
+console.log(2222,global.appOft)
 var InputH = 48;
+var searchBarPaddingTop = 24;
 const styles = StyleSheet.create({
   header: {
-    paddingTop: 24,
+    paddingTop: searchBarPaddingTop,
     paddingBottom: 12,
     paddingRight: 12,
     backgroundColor: 'transparent',
@@ -183,14 +189,16 @@ const styles = StyleSheet.create({
     marginTop: 3,
   },
   closeSelect:{
-    position: 'absolute',
+    // position: 'absolute',
     backgroundColor: '#000',
     marginLeft: 0,
-    marginRight: 0
+    marginRight: 0,
+    marginTop: searchBarPaddingTop - ( Global.os === 'android' ? Global.appOft : 0 )
   },
   selectMask: {
     flex: 1,
-    backgroundColor: 'rgba(255,255,255,0.9)'
+    // backgroundColor: 'rgba(0,0,0,0.9)'
+    backgroundColor: 'rgba(255,255,255,0.8)'
   }
 })
 
