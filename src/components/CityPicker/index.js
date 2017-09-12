7c1a0748994d0f2e11180f0900871b8bff91a6b0
weifan she <923986234@qq.com>
2017-09-12 16:49:32 +0800

    add location

--- src/components/CityPicker/index.js
+++ src/components/CityPicker/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 // import { StyleSheet, Text, View, Image, Dimensions, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';
 import { Picker } from 'antd-mobile';
+import {GetCityMes_get} from '../../api/api/my';
+// import { Constants, Location, Permissions } from 'expo';
+// import { Camera, Permissions } from 'expo';
 import {GetProvinceAndCity_get} from '../../api/api'
 
+import  getLocation  from '../../utils/location';
 class CityPicker extends React.Component {
 
   constructor(props){
@@ -39,6 +43,7 @@ class CityPicker extends React.Component {
         }}
         value={this.state.sValue}
         onChange={v => {
+          // console.log(v);
           this.setState({ sValue: v })
         }}
         { ...this.props }
@@ -51,36 +56,65 @@ class CityPicker extends React.Component {
   }
 
   async componentDidMount(){
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        var initialPosition = JSON.stringify(position);
-        console.log(position)
-      },
-      (error) => {},
-      {enableHighAccuracy: true, timeout: 100000, maximumAge: 1000}
-    );
+    let cityData = null;
+    let cityMes = null;
     try{
       const {Data} = await GetProvinceAndCity_get()
-      const parseData = Data.map(
-        ({Code,Name,City})=>({
-          "value": Code,
-          "label": Name,
-          "children": City.map(
-            (citem)=>({
-              "value": citem.Code,
-              "label": citem.Name,
-            })
-          )
-        })
-      );
+      cityData = Data;
 
-      this.setState({
-        cityData: parseData
-      })
     }catch(e){
       console.log(e);
     }
-    
+
+    try{
+      const {coords} = await getLocation();
+      const { latitude,longitude } = coords;
+
+      const opts = {
+        location:`${latitude}:${longitude}`
+      };
+
+      cityMes = await GetCityMes_get(opts);
+
+    }catch(e){
+      console.log(e);
+    }
+
+
+    if(cityData){
+      const _cityMes = cityMes || { results:[{name:'',path:',,,'}] }
+      const name = _cityMes.results[0].name;
+      const path = _cityMes.results[0].path.split(',');
+      const value = [];
+      const parseData = cityData.map(
+        ({Code,Name,City})=>{
+          if(path[2].match(Name)){
+            value[0] = Code;
+          }
+          return {
+            "value": Code,
+            "label": Name,
+            "children": City.map(
+              (citem)=>{
+                if(name.match(citem.Name)){
+                  value[1] = citem.Code;
+                }
+                return {
+                  "value": citem.Code,
+                  "label": citem.Name,
+                }
+              }
+            )
+          }
+        }
+      );
+      this.setState({
+        cityData: parseData,
+        sValue: value.length === 2 ? value : null
+      })
+
+    }
+
   }
 }
 
