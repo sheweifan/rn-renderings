efc4eb63265b372eeb8cd72589bf7ed229598bd6
weifan she <923986234@qq.com>
2017-09-13 16:27:49 +0800

    add post headers

--- src/api/util.js
+++ src/api/util.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 const instance = axios.create();
 
 function createAPI(baseURL) {
   return function (conf) {
     conf = conf || {};
+
+    if(conf.method === 'post'){
+      conf.headers= {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
     return instance(Object.assign({}, {
       url: conf.url,
       baseURL: baseURL,
