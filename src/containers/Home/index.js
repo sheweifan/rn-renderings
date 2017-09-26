9f41d9a7ca23a8d0416790e4ed1e20d9474a3b1f
weifan she <923986234@qq.com>
2017-09-26 16:02:12 +0800

    use List

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Dimensions, TextInput, Button, ScrollView,TouchableOpacity,FlatList } from 'react-native';
 
-import Img from '../../components/Img';
-import HomeTop from './HomeTop';
+import List from '../../components/List';
 import Imgs from '../../components/Imgs';
-import LoadingState from '../../components/LoadingState';
-
-import icon_index from '../../static/images/nav/icon_index.png'
-import icon_index_active from '../../static/images/nav/icon_index_active.png'
+import HomeTop from './HomeTop';
 
 // import Detail from '../../containers/Detail';
 // import LinearGradient from 'react-native-linear-gradient';
@@ -18,12 +14,13 @@ const {height, width} = Dimensions.get('window');
 import { TXgt_get } from '../../api/api'
 
 
+import icon_index from '../../static/images/nav/icon_index.png'
+import icon_index_active from '../../static/images/nav/icon_index_active.png'
 class Index extends React.Component {
   static navigationOptions = {
     tabBarLabel: '首页',
     tabBarIcon: ({focused}) => {
       // console.log(a)
-
       return <Image
           source={focused?icon_index_active:icon_index}
           style={[{
@@ -37,82 +34,21 @@ class Index extends React.Component {
   };
   constructor(props){
     super(props);
-    this.state = {
-      data: [],
-      loading: true
-    }
   }
   render() {
-    const { data, loading } = this.state;
     const { navigate } = this.props.navigation;
-    
     return (
-      <FlatList 
-        data={data}
+      <List 
         style={styles.container}
+        api={TXgt_get}
         ListHeaderComponent={<HomeTop navigate={navigate} />}
-        keyExtractor={(item, index) => item.EpId}
         renderItem={({item,index})=>{
           return <Imgs key={index} {...item}/>
         }}
-        onEndReached={this.loadMore.bind(this)}
-        ListFooterComponent={<LoadingState loading={loading}/>}
-        refreshing={false}
-        onRefresh={this.refresh.bind(this)}
+        params={{order:1}}
       >
-      </FlatList>
+      </List>
       );
-  }
-  loadData(idx){
-    const { pageIndex, CountPage, state } = this;
-    let { loading,data } = state;
-    // if(loading || pageIndex >= CountPage) {
-    //   loading = null;
-    //   return;
-    // }
-    this.pageIndex = idx;
-    loading = true
-    this.setState({
-      loading
-    })
-    TXgt_get({
-      params: {
-        pageSize: 10,
-        pageIndex: pageIndex
-      }
-    })
-      .then(({Data,CountPage})=>{
-        const newData = data.concat(Data);
-        let loading = false;
-        if(CountPage === idx){
-          loading = null;
-        }
-
-        this.setState({
-          data: newData,
-          loading
-        })
-
-        this.CountPage = CountPage;
-      })
-  }
-  componentDidMount(){
-    this.loadData.bind(this)(1);
-  }
-  refresh(){
-    this.setState({
-      data: [],
-    },()=>this.loadData.bind(this)(1));
-  }
-  loadMore(){
-    const { pageIndex, CountPage, state } = this;
-    let { loading,data } = state;
-    if(loading || loading== null) {
-      loading = null;
-      return;
-    }
-    this.pageIndex = pageIndex+ 1;
-    this.loadData.bind(this)(this.pageIndex);
   }
 }
 // const bannerH = width*250/750;
