11bbfe3e7b68c4567e0bf4a8eb644b332bf8e631
dolor <923986234@qq.com>
2017-10-02 13:30:23 +0800

    add andorid BackHandler

--- src/index.js
+++ src/index.js
@@ -1,113 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { StackNavigator, TabNavigator,addNavigationHelpers } from 'react-navigation';
-import { Provider, connect } from "react-redux";
-
+import { BackHandler } from 'react-native';
+import { addNavigationHelpers, NavigationActions } from 'react-navigation';
+import { Provider, connect } from 'react-redux';
+import { Toast } from 'antd-mobile';
+import AppNavigator from './router'
 import getStore from './store';
 
-
-
 import _g from './config/global.js';
-
-import Index from './containers/Home';
-import Find from './containers/Find';
-// import Detail from './containers/Detail';
-import Search from './containers/Search';
-import MapList from './containers/MapList';
-import FreeDisign from './containers/FreeDisign';
-import DesignerList from './containers/DesignerList';
-import DesignerDetail from './containers/DesignerDetail';
-import ImageView from './containers/ImageView';
-
-import icon_index from './static/images/nav/icon_index.png';
-import icon_index_active from './static/images/nav/icon_index_active.png';
-
-const MainScreenNavigator = TabNavigator(
-  {
-    Index: {screen: Index},
-    Find: {screen: Find},
-  }, 
-  {
-    animationEnabled: false, // 切换页面时不显示动画
-    lazy: true, // 是否懒加载
-    tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
-    swipeEnabled: false, // 禁止左右滑动
-    backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
-    tabBarOptions: {
-      activeTintColor: '#000', // 文字和图片选中颜色
-      inactiveTintColor: '#999', // 文字和图片默认颜色
-      showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
-      indicatorStyle: { height: 0 }, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
-      style: {
-        // paddingBottom: 0,
-        // height: 40,
-        backgroundColor: '#fff', // TabBar 背景色
-        borderTopWidth: 1,
-        borderTopColor: '#ececec',
-        paddingBottom: _g.os === 'android'? 0 : 3,
-        padding: 0,
-        ...(_g.os==='android'?{height: 48}:{})
-      },
-      indicatorStyle:{
-        height: 0
-      },
-      labelStyle: {
-        fontSize: 12, // 文字大小
-        margin: 0,
-      },
-      iconStyle: _g.os === 'android'? {
-        margin:0,
-        padding:0,
-        marginTop: -4
-      }: {}
-    },
-});
-
-const AppNavigator = StackNavigator({
-  Search: { screen: Search },
-  Home: { screen: MainScreenNavigator },
-  ImageView: { screen: ImageView },
-  DesignerDetail: { screen: DesignerDetail },
-  DesignerList: { screen: DesignerList },
-  FreeDisign: { screen: FreeDisign },
-  MapList: { screen: MapList },
-  // Detail: { screen: Detail },
-},{
-  gesturesEnabled: false, // 是否允许右滑返回，在iOS上默认为true，在Android上默认为false
-  navigationOptions:{
-    // header: null,
-    headerBackTitle: null,
-    headerTintColor: '#000000',
-    headerStyle: _g.headerStyle,
-
-    // headerTitleStyle:{
-    //   alignSelf: 'center'
-    //   // display: 'flex',
-    //   // justifyContent: 'center',
-    //   // alignItems: 'center'
-    // },
-  },
-  headerMode: 'screen'
-});
-
-
-const mapStateToProps = (state) => ({
-  nav: state.nav
-});
-
-@connect(mapStateToProps)
-class App extends React.Component {
-    render() {
-        return (
-            <AppNavigator
-                navigation={addNavigationHelpers({
-                    dispatch: this.props.dispatch,
-                    state: this.props.nav
-                })}
-            />
-        );
-    }
-}
 
 const navReducer = (state, action) => {
   const newState = AppNavigator.router.getStateForAction(action, state);
@@ -115,6 +14,49 @@ const navReducer = (state, action) => {
 };
 
 const store = getStore(navReducer);
+
+@connect(state => {
+  return {
+    nav: state.nav
+  }
+})
+class App extends React.Component {
+  componentDidMount() {
+    if(_g.os === 'android'){
+      BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+    }
+  }
+  componentWillUnmount() {
+    if(_g.os === 'android'){
+      BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+    }
+  }
+  onBackPress = () => {
+    const { dispatch, nav } = this.props;
+    if (nav.index === 0 && this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
+      return false;
+    }
+    this.lastBackPressed = Date.now();
+    if(nav.index === 0){
+      Toast.info('再按一次退出应用', 2, ()=>{}, false);
+    }
+    dispatch(NavigationActions.back());
+    return true;
+  };
+  render() {
+    const { dispatch, nav } = this.props;
+    return (
+      <AppNavigator
+          navigation={addNavigationHelpers({
+              dispatch,
+              state: nav
+          })}
+      />
+    );
+  }
+}
+
+
 // export default AppRouter;
 export default function Root() {
     return (
---
+++ src/router/router.js
@@ -0,0 +1,88 @@
+import { StackNavigator, TabNavigator } from 'react-navigation';
+
+import Index from '../containers/Home';
+import Find from '../containers/Find';
+import Search from '../containers/Search';
+import MapList from '../containers/MapList';
+import FreeDisign from '../containers/FreeDisign';
+import DesignerList from '../containers/DesignerList';
+import DesignerDetail from '../containers/DesignerDetail';
+import ImageView from '../containers/ImageView';
+
+import _g from '../config/global.js';
+
+import icon_index from '../static/images/nav/icon_index.png';
+import icon_index_active from '../static/images/nav/icon_index_active.png';
+
+
+const MainScreenNavigator = TabNavigator(
+  {
+    Index: {screen: Index},
+    Find: {screen: Find},
+  }, 
+  {
+    animationEnabled: false, // 切换页面时不显示动画
+    lazy: true, // 是否懒加载
+    tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
+    swipeEnabled: false, // 禁止左右滑动
+    backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
+    tabBarOptions: {
+      activeTintColor: '#000', // 文字和图片选中颜色
+      inactiveTintColor: '#999', // 文字和图片默认颜色
+      showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
+      indicatorStyle: { height: 0 }, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
+      style: {
+        // paddingBottom: 0,
+        // height: 40,
+        backgroundColor: '#fff', // TabBar 背景色
+        borderTopWidth: 1,
+        borderTopColor: '#ececec',
+        paddingBottom: _g.os === 'android'? 0 : 3,
+        padding: 0,
+        ...(_g.os==='android'?{height: 48}:{})
+      },
+      indicatorStyle:{
+        height: 0
+      },
+      labelStyle: {
+        fontSize: 12, // 文字大小
+        margin: 0,
+      },
+      iconStyle: _g.os === 'android'? {
+        margin:0,
+        padding:0,
+        marginTop: -4
+      }: {}
+    },
+});
+
+const Routes = StackNavigator({
+  Home: { screen: MainScreenNavigator },
+  Search: { screen: Search },
+  ImageView: { screen: ImageView },
+  DesignerDetail: { screen: DesignerDetail },
+  DesignerList: { screen: DesignerList },
+  FreeDisign: { screen: FreeDisign },
+  MapList: { screen: MapList },
+  // Detail: { screen: Detail },
+},{
+  gesturesEnabled: false, // 是否允许右滑返回，在iOS上默认为true，在Android上默认为false
+  navigationOptions:{
+    // header: null,
+    headerBackTitle: null,
+    headerTintColor: '#000000',
+    headerStyle: _g.headerStyle,
+
+    // headerTitleStyle:{
+    //   alignSelf: 'center'
+    //   // display: 'flex',
+    //   // justifyContent: 'center',
+    //   // alignItems: 'center'
+    // },
+  },
+  headerMode: 'screen'
+});
+
+export default Routes;
+
+
