6b5438646430fe3f27ed5b9db4d29462a5826842
weifan she <923986234@qq.com>
2017-10-11 09:44:34 +0800

    add navigate

--- src/components/Img/index.js
+++ src/components/Img/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { NavigationActions } from 'react-navigation';
 import { StyleSheet, Text, View, Image, Dimensions, TouchableHighlight } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 const {height, width} = Dimensions.get('window');
 // c8d2c2   736e6b   
 import shadowImg from '../../static/images/itembg.png';
 
-const Img = ({ImageUrl, TitleName, Id, index}) => {
+const Img = ({ImageUrl, TitleName, Id, index, navigate}) => {
   return (
-    <TouchableHighlight>
+    <TouchableHighlight
+      onPress={()=>{ 
+        navigate({
+          routeName: 'ImageView',
+          params: {
+            type: 'img',
+            id: Id
+          }
+        })
+      }}
+    >
       <View style={styles.item}>
         <Image style={styles.itemImg} source={{uri: ImageUrl}} />
         <Image style={styles.itemShadow} source={shadowImg} />
@@ -56,4 +68,13 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Img
+export default connect(
+  (state)=>{
+    return {}
+  },
+  (dispatch)=>{
+    return {
+      navigate: (path)=>dispatch(NavigationActions.navigate(path))
+    }
+  }
+)(Img);
